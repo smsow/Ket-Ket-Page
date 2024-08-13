c51ea3737ec5ba60c3e44c3e5d976d930247ca13
smsow <sicksow12@gmail.com>
2024-08-13 17:48:23 +0000

    plus

--- frontend/src/components/components_search_page/map.jsx
+++ frontend/src/components/components_search_page/map.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-// Coordinates for Dakar, Senegal
-const position = [14.6928, -17.4467];
+// Coordinates for the map center (example: Toulouse, France)
 
-export default function Map() {
+export default function Map({latitude=14.6928, longitude=-17.4467}) {
+    const position = [latitude, longitude ];
     return (
-        <MapContainer center={position} zoom={12} style={{ height: '100vh', width: '100%' }}>
+        <MapContainer center={position} zoom={30} style={{ height: '100vh', width: '100%' }}>
             <TileLayer
                 url='https://tile.jawg.io/jawg-lagoon/{z}/{x}/{y}{r}.png?access-token=2U5baWI92SCFF5D9Gp53vRanR2r9g5TQ6X5qhEY4Z0tIQUijlbOEbW2eZmOLGfx9'
                 attribution='<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
             <Marker position={position}>
                 <Popup>
-                    Dakar, Senegal.<br />A vibrant city with rich culture.
+                    Example Location.<br />Description here.
                 </Popup>
             </Marker>
         </MapContainer>
